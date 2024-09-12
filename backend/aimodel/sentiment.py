adcb8061a2d83d3059dca41bcbdff5c21c040ec4
Rakesh Venkatesh <rakesh.venkatesh@nl.abnamro.com>
2024-09-12 21:57:46 +0200

    Revert "commit"
    This reverts commit eaf811d8bc82fc40edaa7bb97827506d43cfa650.

--- backend/aimodel/sentiment.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-class SentimentAnalyzer:
-    def __init__(self):
-        self.pipeline = pipeline("text-classification", model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
-    def analyze(self, text):
-        result = self.pipeline(text)[0]
-        return result['label'], result['score']
