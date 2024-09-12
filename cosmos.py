45ae63d2f9f633aa94bf7eced087f5bb7aa74f23
Rakesh Venkatesh <rakesh.venkatesh@nl.abnamro.com>
2024-09-12 16:31:41 +0200

    read from cosmos

---
+++ cosmos.py
@@ -0,0 +1,20 @@
+from azure.cosmos import CosmosClient
+import os
+
+
+# Initialize the Cosmos client
+endpoint = os.environ.get('ACCOUNT_HOST')
+key = os.environ.get('ACCOUNT_KEY')
+client = CosmosClient(endpoint, key)
+
+# Connect to the database and container
+database_name = os.environ.get('COSMOS_DATABASE')
+container_name = os.environ.get('COSMOS_CONTAINER')
+database = client.get_database_client(database_name)
+container = database.get_container_client(container_name)
+
+
+def data():
+    query = "SELECT * FROM c"
+    items = list(container.query_items(query=query, enable_cross_partition_query=True))
+    return (items)
