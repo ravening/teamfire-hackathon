0a219db3cf9a6505ecf0a9a2265888215d002f21
Rakesh Venkatesh <rakesh.venkatesh@nl.abnamro.com>
2024-09-13 12:14:21 +0200

    new endpoint

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ async def index(request: Request):
     return templates.TemplateResponse('index.html', {"request": request})
 
 
+@app.get("/chat", response_class=HTMLResponse)
+async def index(request: Request):
+    print('Request for index page received')
+    return templates.TemplateResponse('chat.html', {"request": request})
+
+
 @app.get("/read")
 async def index(request: Request):
     logger.error('Request for read all items received')
@@ -53,7 +59,6 @@ async def favicon():
 async def hello(request: Request, name: str = Form(...)):
     global customer_data
     if name:
-        logger.error(f"getting the request: {name}")
         name = chat_with_ai("unique_conversation_id", name)
         return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
     else:
@@ -61,14 +66,14 @@ async def hello(request: Request, name: str = Form(...)):
         return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
 
 
-# @app.post('/hello', response_class=HTMLResponse)
-# async def hello(request: Request, name: str = Form(...)):
-#     if name:
-#         name = chat.chat_with_ai(name)
-#         return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
-#     else:
-#         print('Request for hello page received with no name or blank name -- redirecting')
-#         return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+@app.post('/chatresponse')
+async def chatresponse(request: Request, name: str = Form(...)):
+    if name:
+        response = chat.chat_with_ai(name)
+        return response
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
 
 
 if __name__ == '__main__':
